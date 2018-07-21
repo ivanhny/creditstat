931e532bd87e677dd15280dcf4236b40b59f9321
Ivan H <ivanhny@Ivans-Mac-mini.local>
2018-07-21 03:42:39 -0400

    Added Test data

---
+++ enterdata.php
@@ -0,0 +1,27 @@
+<?php
+
+$connect = new mysqli_connect(localhost, 'root', 'pushi100', 'Credit');
+
+if(!$connect) {
+   echo "Error: Unable to Connect to database";
+   echo "Debugging Error: ".mysqli_connect_error().PHP_EOL;
+   exit;
+}
+
+echo "Success: A connection was enabled";
+echo "Host Information: ".mysqli_get_host_info($connect).PHP_EOL;
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Data Entry for Credit Payment Stat</title>
+</head>
+<body>
+  <p> Body of Doc</p>
+
+
+</body>
+</html>
