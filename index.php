300748c9f46aea5c6e3b001557f9ff58637a341c
Ivan H <ivanhny@Ivans-Mac-mini.local>
2018-07-21 02:30:17 -0400

    nitial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "PHP Working";
+
+
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <title>Credit Card Payments Due</title>
+  <meta name="Generator" content="Cocoa HTML Writer">
+  <meta name="CocoaVersion" content="1561.4">
+  <style type="text/css">
+    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; line-height: 14.0px; font: 12.0px Helvetica; color: #000000; -webkit-text-stroke: #000000}
+    span.s1 {font-kerning: none}
+  </style>
+</head>
+<body>
+<p class="p1"><span class="s1">&lt;title&gt;Test.Com&lt;/title&gt;</span></p>
+<p class="p1"><span class="s1">&lt;p&gt; test.com!&lt;/p&gt;</span></p>
+</body>
+</html>
